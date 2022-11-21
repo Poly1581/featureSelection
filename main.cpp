50aebbc254f53f61cdd22ce143332b9be0e22a5c
Poly1581 <bpaul006@ucr.edu>
2022-11-20 18:47:18 -0800

    switched to using set of features

--- main.cpp
+++ main.cpp
@@ -1,24 +1,20 @@
 #include <vector>
 using std::vector;
+#include <set>
+using std::set;
 #include <string>
 using std::string;
 #include <iostream>
-using std::cin;
-using std::cout;
-using std::endl;
+using std::cin, std::cout, std::endl;
 #include <fstream>
 using std::ifstream;
 #include <sstream>
-using std::getline;
-using std::istringstream;
+using std::getline, std::istringstream;
 #include <cmath>
 using std::round;
 #include <algorithm>
-using std::min;
-using std::find;
+using std::min, std::find;
 #include <limits>
-#include <random>
-#include <chrono>
 
 //Struct to store label and features of labelled data seperately
 struct labelledData {
@@ -61,10 +57,10 @@ vector<labelledData> loadData(string& fileName) {
 
 
 //Calculate euclidean distance between two points for given features
-double distance(vector<double> a, vector<double> b, vector<int> features) {
+double distance(vector<double> a, vector<double> b, set<int> features) {
 	double distance = 0;
 	for(int i = 0; i < a.size(); i++) {
-		if(find(features.begin(),features.end(),i) != features.end()) {
+		if(features.count(i) != 0) {
 			distance += pow(a.at(i)-b.at(i),2);
 		}
 	}
@@ -72,7 +68,7 @@ double distance(vector<double> a, vector<double> b, vector<int> features) {
 }
 
 //Find label of nearest neighbor in data
-bool nearestNeighbor(vector<labelledData> data, labelledData instance, vector<int> features) {
+bool nearestNeighbor(vector<labelledData> data, labelledData instance, set<int> features) {
 	double minDistance = std::numeric_limits<double>::max();
 	bool label = false;
 	for(labelledData neighbor:data) {
@@ -89,7 +85,7 @@ bool nearestNeighbor(vector<labelledData> data, labelledData instance, vector<in
 }
 
 //Evaluate feature accuracy
-double leaveOneOut(vector<labelledData> data, vector<int> features) {
+double leaveOneOut(vector<labelledData> data, set<int> features) {
 	int correct = 0;
 	for(labelledData instance:data) {
 		if(instance.label == nearestNeighbor(data,instance,features)) {
@@ -99,72 +95,23 @@ double leaveOneOut(vector<labelledData> data, vector<int> features) {
 	return correct/(double)data.size();
 }
 
-void displayVector(vector<int> features) {
-	cout << "{";
-	for(int f = 0; f < features.size()-1; f++) {
-		cout << features.at(f) << ", ";
-	}
-	cout << features.back() << "}";
-}
-
-void displayResult(vector<int> features, double accuracy) {
-	cout << "\tAccuracy using feature(s) ";
-	displayVector(features);
-	cout << " was " << round(accuracy*1000)/10 << endl;
-}
-
-void displayBest(vector<int> features, double accuracy) {
-	cout << "Accuracy of feature set ";
-	displayVector(features);
-	cout << " was best (" << round(accuracy*1000)/10 << ")" << endl;
-}
-
-//start with emtpy feature set and add
+//construct empty features set and add
 void forwardSelection(vector<labelledData> data) {
-	vector<int> dataFeatures = {};
-	for(int f = 0; f < data.front().features.size(); f++) {
-		dataFeatures.push_back(f);
-	}
-	vector<int> features = {};
-	double overallBestAccuracy = -1;
-	vector<int> overallBestFeatures = {};
-	while(features.size() < min((int)dataFeatures.size(),10)) {
-		double bestAccuracy = -1;
-		int bestFeature = -1;
-		for(int feature:dataFeatures) {
-			if(find(features.begin(), features.end(), feature) == features.end()) {
-				vector<int> newFeatures = features;
-				newFeatures.push_back(feature);
-				double accuracy = leaveOneOut(data,newFeatures);
-				displayResult(newFeatures,accuracy);
-				if(accuracy > bestAccuracy) {
-					bestAccuracy = accuracy;
-					bestFeature = feature;
-				}
-			}
-		}
-		features.push_back(bestFeature);
-		displayBest(features,bestAccuracy);
-		if(bestAccuracy > overallBestAccuracy) {
-			overallBestAccuracy = bestAccuracy;
-			overallBestFeatures = features;
-		}
-	}
-	displayBest(overallBestFeatures,overallBestAccuracy);
+	set<int> features;
+
 }
 
-//start with full feature set and remove
+//construct full feature set and remove
 void backwardElimination(vector<labelledData> data) {
-
+	set<int> features;
 }
 
+//small 61, large 33
 int main() {
 	cout << "Enter the name of the file containing the data which you wish to analyze" << endl;
 	string fileName  = "";
 	cin >> fileName;
 	vector<labelledData> data = loadData(fileName);
-	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-	std::shuffle(data.begin(), data.end(), std::default_random_engine(seed));
 	cout << "FILE HAS " << data.size() << " INSTANCES" << endl;
 	forwardSelection(data);
 	// for(labelledData instance:data) {
